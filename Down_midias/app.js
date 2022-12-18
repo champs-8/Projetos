669ce50dd6f209b8bbc25888f61d34c9d3459341
champs-8 <https://github.com/champs-8/Projetos>
2022-12-18 15:38:08 -0300

    so passei raiva

--- Down_midias/app.js
+++ Down_midias/app.js
@@ -1,5 +1,7 @@
 //npm i express
 const express = require("express");
+//npm i fs
+const fs = require("fs");
 //npm i ytdl-core
 const ytdl = require('ytdl-core')
 //instanciando a funcão express
@@ -10,17 +12,28 @@ const port = 3600;
 const cors = require("cors");
 app.use(cors());
 
-
 //ROTAS
 
 //qual pagina quero abrir
 app.get("/", (req, res) => {
-    const { url } = req.query;
-    let date = Date.now();
-    //cabeçalho meta dados 
-    res.header('Content-Disposition', `attachmentt; filename=Video${date}.mp4`)
-    return ytdl(url, { filter: "videoandaudio"}).pipe(res);
+    res.sendFile(`${__dirname}/index.html`);
+    //nao sei porquê funcionou, mas carregou o css
+    app.use('/Down_midias/public', express.static('public'));
+    
+    //exemplo
+
+    // const { url } = req.query;
+    //let date = Date.now();
+
+    // cabeçalho meta dados 
+
+    // res.header('Content-Disposition', `attachmentt; filename=Video${date}.mp4`)
+    // return ytdl(url, { filter: "videoandaudio"}).pipe(res);    
+    ytdl('https://www.youtube.com/shorts/LioK54DvSjo', {filter: "videoandaudio"}).pipe(fs.createWriteStream('Video.mp4'));
 });
+
+
+
 //iniciando o servidor 
-app.listen(port, () => console.log(`http://localhost:${port}`));
+app.listen(port, () => console.log(`*************\n\nhttp://localhost:${port}\n\n*************`));
 
---
+++ Down_midias/scritp.js
@@ -0,0 +1,16 @@
+const url = document.getElementById('txturl');
+let radioCheck;
+
+//saber qual formato foi selectionado
+function check(id) {
+    if (id === 'mp3') {
+        radioCheck = id;
+
+        /*aqui vou fazer a incrementação dos formatos*/
+    }
+    return radioCheck = id
+} 
+
+function down() {
+    alert('ola mundo')
+}
